3e47e0e328a2e6530f0e33fa620b85ce643d6a17
Mike Baynton <mike@mbaynton.com>
2020-06-01 19:25:38 -0500

    Feature complete
    Ready for testing and review

--- src/Commands/HTTPS/ChallengeCommand.php
+++ src/Commands/HTTPS/ChallengeCommand.php
@@ -52,7 +52,7 @@ class ChallengeCommand extends TerminusCommand implements SiteAwareInterface
           $this->log()->notice('Wrote ACME challenge to file {filename}', compact('filename'));
           $this->log()->notice('Please copy this file to your web server so that it will be served from the URL');
           $this->log()->notice('http://{domain}{path}', ['domain' => $domain, 'path' => $data->verification_key]);
-          $this->log()->notice('After this is complete, run terminus acme-file-verify.');
+          $this->log()->notice('After this is complete, run {command}', ['command' => "terminus acme-file-verify $site_env $domain"]);
         } else {
           throw new TerminusException('Failed writing to {filename}', compact('filename'));
         }
@@ -82,18 +82,32 @@ class ChallengeCommand extends TerminusCommand implements SiteAwareInterface
      */
     public function getChallengeDNStxt($site_env, $domain, $options = ['format' => 'list'])
     {
-        list($data, $acmeStatus) = $this->getACMEStatus($site_env, $domain);
-        if (!$acmeStatus) {
-            return;
-        }
+      list($data, $acmeStatus) = $this->getACMEStatus($site_env, $domain);
+      if (!$acmeStatus) {
+        return;
+      }
 
-        // Sanity check: this should never happen, as getACMEStatus should throw
-        // in any instance where there is no verification dns txt record.
-        if (empty($data->{'dns-01'})) {
-            throw new TerminusException('No DNS txt record challenge information available for domain {domain}.', compact('status', 'domain'));
+      // Sanity check: this should never happen, as getACMEStatus should throw
+      // in any instance where there is no verification dns txt record.
+      if (empty($data->{'dns-01'})) {
+        throw new TerminusException('No DNS txt record challenge information available for domain {domain}.', compact('status', 'domain'));
+      }
+      $data = $data->{'dns-01'};
+      $struct = ChallengeCommand::formatChallengeDNStxt($domain, $data);
+      $txt_record = $struct[0];
+      $txt_record_components = $struct[1];
+      // Provide instructions in a log message when the format is 'list'
+        // n.b. 'list' format prints out each line's key, which for this
+        // command is the full dns txt record.
+        if ($options['format'] == 'list') {
+          $this->log()->notice("Create a DNS txt record containing:\n$txt_record\n");
+          $this->log()->notice('After this is complete, run {command}', ['command' => "terminus acme-dns-verify $site_env $domain"]);
+        } else {
+          return new RowsOfFields([$txt_record => $txt_record_components]);
         }
-        $data = $data->{'dns-01'};
+    }
 
+    public static function formatChallengeDNStxt($domain, $data) {
         $txt_record_components = [
             'domain' => $domain,
             'record-name' => $data->verification_key,
@@ -106,13 +120,7 @@ class ChallengeCommand extends TerminusCommand implements SiteAwareInterface
         $dns_txt_record_tmpl = 'record-name ttl class record-type "text-data"';
         $txt_record = str_replace(array_keys($txt_record_components), array_values($txt_record_components), $dns_txt_record_tmpl);
 
-        // Provide instructions in a log message when the format is 'list'
-        // n.b. 'list' format prints out each line's key, which for this
-        // command is the full dns txt record.
-        if ($options['format'] == 'list') {
-            $this->log()->notice('Create a DNS txt record containing:', compact('contents', 'domain'));
-        }
-        return new RowsOfFields([$txt_record => $txt_record_components]);
+        return [$txt_record, $txt_record_components];
     }
 
     /**
@@ -144,6 +152,10 @@ class ChallengeCommand extends TerminusCommand implements SiteAwareInterface
         if ($status == 'not_required') {
             $this->log()->notice('Domain verification for {domain} is not necessary; https has not been configured for this domain in its current location.', compact('domain'));
             return [null, false];
+        }
+
+        if ($status == 'unavailable' && !empty($ownership->message)) {
+          throw new TerminusException($ownership->message);
         }
 
         if ($status != 'required') {
--- src/Commands/HTTPS/ChallengeVerifyCommand.php
+++ src/Commands/HTTPS/ChallengeVerifyCommand.php
@@ -34,12 +34,30 @@ class ChallengeVerifyCommand extends TerminusCommand implements SiteAwareInterfa
     $this->verifyChallenge($site_env, $domain, 'http-01');
   }
 
+  /**
+   * Triggers acceptance of the ACME dns-01 challenge returned by alpha:https:challenge:dns
+   * and prints whether ownership of the domain was successfully proven.
+   *
+   * @authorize
+   *
+   * @command alpha:https:challenge:dns-txt:verify
+   * @aliases acme-dns-verify
+   *
+   * @param string $site_env Site & environment in the format `site-name.env`
+   * @param string $domain The domain name to verify.
+   * @usage <site>.<env> <domain> Verifies your ownership of <domain> by querying for a DNS TXT record.
+   */
+  public function verifyDnsChallenge($site_env, $domain)
+  {
+    $this->verifyChallenge($site_env, $domain, 'dns-01');
+  }
+
   protected function verifyChallenge($site_env, $domain, $challenge_type) {
     list(, $env) = $this->getSiteEnv($site_env);
 
     $domains = $env->getDomains();
 
-    // Check if it's already been verified.
+    // Check if it's already been verified, and note current challenges.
     $data = GetACMEStatus::get($domains, $domain);
     // This happens if the domain status object could not be built by the deadline.
     // In this case, launch ownership verification, but we may not be able to poll.
@@ -49,6 +67,11 @@ class ChallengeVerifyCommand extends TerminusCommand implements SiteAwareInterfa
       $preprovision_result = $data->{'ownership_status'};
       $preprovision_result = $preprovision_result->{'preprovision_result'};
       $status = $preprovision_result->status;
+    }
+
+    $current_challenge = '';
+    if (!empty($data->acme_preauthorization_challenges)) {
+      $current_challenge = $data->acme_preauthorization_challenges->$challenge_type->verification_value;
     }
 
     switch ($status) {
@@ -96,7 +119,8 @@ class ChallengeVerifyCommand extends TerminusCommand implements SiteAwareInterfa
       $status = $preprovision_result->status;
       switch ($status) {
         case 'failed':
-          $this->handleVerificationFailed();
+          $this->handleVerificationFailed($site_env, $domain, $current_challenge, $data, $challenge_type);
+          return;
         case 'success':
           $this->log()->notice('Ownership verification is complete!');
           $this->log()->notice('Your HTTPS certificate will be deployed to Pantheon\'s Global CDN shortly.');
@@ -104,13 +128,75 @@ class ChallengeVerifyCommand extends TerminusCommand implements SiteAwareInterfa
       }
     }
 
-    $this->handleVerificationFailed();
+    $this->handleVerificationFailed($site_env, $domain, $current_challenge, $data, $challenge_type);
   }
 
-  protected function handleVerificationFailed() {
-    $this->log()->notice('Double-check that your challenge is being served correctly.');
-    $this->log()->notice('See {link} for assistance', ['link' => 'https://pantheon.io/docs/guides/launch/domains']);
-    $this->log()->notice('or contact Pantheon Support. You may try again up to 5 times per hour.');
+  protected function handleVerificationFailed($site_env, $domain, $current_challenge, $data, $challenge_type) {
+    // Display rich error information if we have any.
+    $preprovision_result = $data->{'ownership_status'};
+    $preprovision_result = $preprovision_result->{'preprovision_result'};
+    $pantheon_docs = 'https://pantheon.io/docs/guides/launch/domains';
+    $support_ref = '';
+    if (!empty($preprovision_result->last_preprovision_problem)) {
+      $problem = $preprovision_result->last_preprovision_problem;
+      if (!empty($problem->PantheonDocsLink)) {
+        $pantheon_docs = $problem->PantheonDocsLink;
+      }
+      if (!empty($problem->SupportReference)) {
+        $support_ref = " with reference \"" . $problem->SupportReference . '"';
+      }
+      if (!empty($problem->PantheonTitle)) {
+        $this->log()->notice($problem->PantheonTitle);
+      }
+      if (!empty($problem->PantheonDetail)) {
+        $this->log()->notice($problem->PantheonDetail);
+      }
+      if (!empty($problem->PantheonActionItem)) {
+        $this->log()->notice($problem->PantheonActionItem);
+      }
+
+      if (!empty($problem->Detail) || !empty($problem->ProblemType)) {
+        $this->log()->notice('');
+        $detail = '';
+        if (!empty($problem->ProblemType)) {
+          $detail = $detail . "\n" . $problem->ProblemType;
+        }
+        if (!empty($problem->Detail)) {
+          $detail = $detail . "\n" . $problem->Detail;
+        }
+        $this->log()->notice("Raw verification result:$detail");
+      }
+    } else {
+      $this->log()->notice('Double-check that your challenge is being served correctly.');
+    }
+
+    $this->log()->notice('See {link} for assistance', ['link' => $pantheon_docs]);
+    $this->log()->notice("or contact Pantheon Support$support_ref.");
+
+    // Warn if ownership verification has become unavailable.
+    // (Typically user has attempted more times than LE allows per hour)
+    if ($data->ownership_status->status == 'unavailable' && !empty($data->ownership_status->message)) {
+      $this->log()->warning($data->ownership_status->message);
+    }
+
+    // Warn if the challenge had to be changed.
+    if (!empty($data->acme_preauthorization_challenges)) {
+      $new_challenge = $data->acme_preauthorization_challenges->$challenge_type->verification_value;
+      if ($new_challenge != $current_challenge) {
+        $this->log()->warning('The old challenge cannot be tried again.');
+        if ($challenge_type == 'dns-01') {
+          $struct = ChallengeCommand::formatChallengeDNStxt($domain, $data->acme_preauthorization_challenges->{'dns-01'});
+          $txt_record = $struct[0];
+          $this->log()->warning("Please update your DNS to serve the new challenge below:\n$txt_record");
+        }
+        if ($challenge_type == 'http-01') {
+          $this->log()->warning('Please run {command} again to obtain a new challenge file.',
+            ['command' => "terminus alpha:https:challenge:file $site_env $domain"]
+          );
+        }
+      }
+    }
+
     throw new TerminusException('Ownership verification was not successful.');
   }
 }
